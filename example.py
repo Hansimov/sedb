f6d68d28042ecfada466c951ceee328f4e045f6e
Hansimov <591172499@qq.com>
2024-09-22 17:48:01 +0800

    :pencil: [Doc] Readme: add install and usage examples

---
+++ example.py
@@ -0,0 +1,19 @@
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent / "src"))
+
+import bldb
+
+from bldb import MongoOperator, MongoConfigsType
+
+
+if __name__ == "__main__":
+    mongo_configs = {
+        "host": "localhost",
+        "port": 27017,
+        "dbname": "test",
+    }
+    mongo = MongoOperator(configs=mongo_configs)
+
+    # python example.py
