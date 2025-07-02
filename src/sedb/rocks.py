7439c65b8847d165560250c730fbb2e567ad6689
Hansimov <591172499@qq.com>
2025-07-02 23:07:50 +0800

    :zap: [Enhance] RocksOperator: improve options and add notes of ulimit to resolve write stall issue

--- src/sedb/rocks.py
+++ src/sedb/rocks.py
@@ -1,4 +1,4 @@
-from rocksdict import Rdict, Options
+from rocksdict import Rdict, Options, WriteOptions
 
 from pathlib import Path
 from tclogger import logger, logstr, get_now_str, brk
@@ -7,8 +7,11 @@ from typing import TypedDict, Union
 
 class RocksConfigsType(TypedDict):
     db_path: Union[str, Path]
+    max_open_files: int = 20000
     target_file_size_base_mb: int = 64
     write_buffer_size_mb: int = 64
+    level_zero_slowdown_writes_trigger: int = 20000
+    level_zero_stop_writes_trigger: int = 50000
 
 
 class RocksOperator:
@@ -17,6 +20,11 @@ class RocksOperator:
 
     RocksDB include headers:
     * https://github.com/facebook/rocksdb/blob/10.4.fb/include/rocksdb/db.h
+
+    Write Stalls · facebook/rocksdb Wiki
+    * https://github.com/facebook/rocksdb/wiki/Write-Stalls
+
+    NOTE: Run `ulimit -n 20000` to increase the max open files limit system-wide
     """
 
     def __init__(
@@ -39,18 +47,33 @@ class RocksOperator:
             self.connect(connect_msg=connect_msg)
 
     def init_configs(self):
+        # init db_path
         self.db_path = Path(self.configs["db_path"])
+
+        # init db options
         options = Options(raw_mode=self.raw_mode)
         options.create_if_missing(True)
         options.set_max_file_opening_threads(128)
         options.set_max_background_jobs(128)
+        options.set_max_open_files(self.configs.get("max_open_files", 20000))
         options.set_target_file_size_base(
             self.configs.get("target_file_size_base_mb", 64) * 1024 * 1024
         )
         options.set_write_buffer_size(
             self.configs.get("write_buffer_size_mb", 64) * 1024 * 1024
         )
+        options.set_level_zero_slowdown_writes_trigger(
+            self.configs.get("level_zero_slowdown_writes_trigger", 20000)
+        )
+        options.set_level_zero_stop_writes_trigger(
+            self.configs.get("level_zero_stop_writes_trigger", 50000)
+        )
         self.db_options = options
+
+        # init write options
+        write_options = WriteOptions()
+        write_options.no_slowdown = True
+        self.write_options = write_options
 
     def connect(self, connect_msg: str = None):
         db_str = logstr.mesg(brk(self.db_path))
@@ -66,6 +89,7 @@ class RocksOperator:
             else:
                 status = "Opened"
             self.db = Rdict(path=str(self.db_path.resolve()), options=self.db_options)
+            self.db.set_write_options(self.write_options)
             if self.verbose:
                 count = self.get_total_count()
                 count_str = f"{count} keys"
