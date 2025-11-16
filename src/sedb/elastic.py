ef1220c49de956ee9064e54f911d922349b6a115
Hansimov <591172499@qq.com>
2025-11-16 17:24:42 +0800

    :zap: [Enhance] ElasticOperator: add retry in connect to resolve elastic init issue

--- src/sedb/elastic.py
+++ src/sedb/elastic.py
@@ -1,5 +1,6 @@
 from elasticsearch import Elasticsearch
-from tclogger import logger
+from tclogger import logger, logstr
+from time import sleep
 from typing import TypedDict
 
 from .message import ConnectMessager
@@ -46,6 +47,28 @@ class ElasticOperator:
         self.api_key = self.configs["api_key"]
         self.endpoint = f"https://{self.host}:{self.port}"
 
+    def log_connect(self):
+        self.msgr.log_endpoint()
+        self.msgr.log_now()
+        self.msgr.log_msg()
+
+    def connect_client(self):
+        self.client = Elasticsearch(
+            hosts=self.endpoint,
+            ca_certs=self.ca_certs,
+            api_key=self.api_key,
+            # basic_auth=(self.username, self.password),
+        )
+        if self.verbose:
+            client_info = self.client.info() or {}
+            client_info_str = str(dict(self.client.info() or {}))
+            status_str = "✓ Connected:"
+            if client_info:
+                logger.okay(status_str)
+            else:
+                logger.warn(status_str)
+            logger.mesg(f"  * {client_info_str}", indent=self.indent)
+
     def connect(self, connect_msg: str = None):
         """Connect to self-managed cluster with API Key authentication
         * https://www.elastic.co/guide/en/elasticsearch/client/python-api/current/connecting.html#auth-apikey
@@ -58,24 +81,19 @@ class ElasticOperator:
         Connect to self-managed cluster with HTTP Bearer authentication
         * https://www.elastic.co/guide/en/elasticsearch/client/python-api/current/connecting.html#auth-bearer
         """
-        self.msgr.log_endpoint()
-        self.msgr.log_now()
-        self.msgr.log_msg()
-        try:
-            self.client = Elasticsearch(
-                hosts=self.endpoint,
-                ca_certs=self.ca_certs,
-                api_key=self.api_key,
-                # basic_auth=(self.username, self.password),
-            )
-            if self.verbose:
-                client_info = self.client.info() or {}
-                client_info_str = str(dict(self.client.info() or {}))
-                status_str = "✓ Connected:"
-                if client_info:
-                    logger.okay(status_str)
-                else:
-                    logger.warn(status_str)
-                logger.mesg(f"  * {client_info_str}", indent=self.indent)
-        except Exception as e:
-            raise e
+        self.log_connect()
+        retry_count = 0
+        retry_interval = 30
+        max_retries = 10
+        while retry_count < max_retries:
+            try:
+                self.connect_client()
+                break
+            except Exception as e:
+                retry_count += 1
+                retry_str = f"{logstr.file(retry_count)}/{logstr.mesg(max_retries)}"
+                logger.warn(f"  × [{retry_str}] connect failed: {e}")
+                if retry_count >= max_retries:
+                    raise e
+                logger.note(f"  * Re-connect in {retry_interval} seconds ...")
+                sleep(retry_interval)
